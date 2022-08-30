d9194d96c07ca7a9d52c413270bb85955d4d5604
Thejohnes <japajlc@gmail.com>
2022-08-30 16:28:40 -0300

    variaveis inseridas

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,8 @@ let email
 let profissao
 let idade
 let lugarDosSonhos
+seuNome = "João Luiz Carneiro"
+email = "japajlc@gmail.com"
+profissao = "Engenheiro"
+idade = 34
+lugarDosSonhos = "Maldivas"
